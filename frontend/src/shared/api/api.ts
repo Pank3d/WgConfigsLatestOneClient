dfcbf1024e1e4184536652312c1010c817704419
m.jilevskiy <m.jilevskiy@aerodisk.ru>
2026-03-07 17:24:49 +0300

    feat: add https

--- frontend/src/shared/api/api.ts
+++ frontend/src/shared/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '@/entities/user';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001/api',
+  baseURL: import.meta.env.VITE_API_URL || '/api',
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
